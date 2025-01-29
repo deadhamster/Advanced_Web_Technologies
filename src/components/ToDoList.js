d7dd6e3ef2a8202f732e36a538437a998f0ecfa3
deadhamster <psunghyo04@gmail.com>
2025-01-29 11:54:31 +0500

    Step 9: Using map method for array

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -2,22 +2,15 @@ import React from "react";
 import ToDoListItem from "./ToDoListItem";
 
 const ToDoList = ({ todos }) => {
-  return (
-    <ul>
+  const elements = todos.map((item) => {
+    return (
       <li>
-        <ToDoListItem label={todos[0].label} important={todos[0].important} />
+        <ToDoListItem label={item.label} important={item.important} />
       </li>
-      <li>
-        <ToDoListItem label={todos[1].label} important={todos[1].important} />
-      </li>
-      <li>
-        <ToDoListItem label={todos[2].label} important={todos[2].important} />
-      </li>
-      <li>
-        <ToDoListItem label={todos[3].label} important={todos[3].important} />
-      </li>
-    </ul>
-  );
+    );
+  });
+
+  return <ul>{elements}</ul>;
 };
 
 export default ToDoList;
