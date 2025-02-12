98ba8dee516720be885d4eae411dcb4fb740ab21
deadhamster <psunghyo04@gmail.com>
2025-02-12 11:20:29 +0500

    Step 26: Creating newArray

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -21,10 +21,7 @@ export default class App extends Component {
     this.setState(({ todoData }) => {
       const idx = todoData.findIndex((element) => element.id === id);
 
-      todoData.splice(id, 1);
-      const before = todoData.slice(0, idx);
-      const after = todoData.slice(idx + 1);
-      const newArray = [...before, ...after];
+      const newArray = [...todoData.slice(0, idx), ...todoData.slice(idx + 1)];
 
       return {
         todoData: newArray,
