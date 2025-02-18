fca416fecde1119b280a16644b2d5f96d2932166
deadhamster <psunghyo04@gmail.com>
2025-02-19 00:53:49 +0500

    Step 32: Upgrading ItemAddForm

--- src/components/item_add_form/ItemAddForm.js
+++ src/components/item_add_form/ItemAddForm.js
@@ -1,16 +1,41 @@
 import React, { Component } from "react";
 import "./ItemAddForm.css";
 export default class ItemAddForm extends Component {
+  state = {
+    label: "",
+  };
+
+  onLabelChange = (e) => {
+    this.setState({
+      label: e.target.value,
+    });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.onItemAdded(this.state.label);
+  };
+
   render() {
     return (
-      <div className={"item-add-form"}>
-        <button
-          className={"btn btn-outline-secondary"}
-          onClick={() => this.props.onItemAdded("New Item Added")}
-        >
-          Add Item
-        </button>
-      </div>
+      // <div className={"item-add-form"}>
+      //   <button
+      //     className={"btn btn-outline-secondary"}
+      //     onClick={() => this.props.onItemAdded("New Item Added")}
+      //   >
+      //     Add Item
+      //   </button>
+      // </div>
+
+      <form className={"item-add-form d-flex"} onSubmit={this.onSubmit}>
+        <input
+          type={"text"}
+          className={"form-control"}
+          onChange={this.onLabelChange}
+          placeholder={"Type for To Do"}
+        />
+        <button className={"btn btn-outline-secondary"}>Add Item</button>
+      </form>
     );
   }
 }
